826023bcb8920d8bb882923b2b8db6a269c291a7
voquoctuan <voquoctuanvfedu@gmail.com>
2022-02-18 11:44:08 +0700

    bai tap kieu so

---
+++ kieuso.java
@@ -0,0 +1,55 @@
+/*
+import java.math.MathContext;
+import java.util.Scanner;
+public class kieuso {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        byte a = sc.nextByte();
+        byte b = sc.nextByte();
+        System.out.println("a + b = " + (a+b));
+        System.out.println("a - b = " + (a-b));
+        System.out.println("a * b = " + (a*b));
+        System.out.println("a / b = " + (a/b));
+        System.out.println("a % b = " + (a%b));
+        System.out.println("a ^ b = " + Math.pow(a, b));
+    }
+}
+
+
+
+
+import java.math.MathContext;
+import java.util.Scanner;
+public class kieuso {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        System.out.println("a + b = " + (a+b));
+        System.out.println("a - b = " + (a-b));
+        System.out.println("a * b = " + (a*b));
+        System.out.println("a / b = " + (a/b));
+        System.out.println("a % b = " + (a%b));
+        System.out.println("a ^ b = " + Math.pow(a, b));
+    }
+}
+*/
+
+
+
+
+import java.math.MathContext;
+import java.util.Scanner;
+public class kieuso {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double a = sc.nextDouble();
+        double b = sc.nextDouble();
+        System.out.println("a + b = " + (a+b));
+        System.out.println("a - b = " + (a-b));
+        System.out.println("a * b = " + (a*b));
+        System.out.println("a / b = " + (a/b));
+        System.out.println("a % b = " + (a%b));
+        System.out.println("a ^ b = " + Math.pow(a, b));
+    }
+}
