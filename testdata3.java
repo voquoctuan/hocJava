8b5b6c99d98625dad9115ae2a60b053b5e962a12
voquoctuan <voquoctuanvfedu@gmail.com>
2022-03-04 07:26:22 +0700

    reads a floating-point number

---
+++ testdata3.java
@@ -0,0 +1,45 @@
+import java.util.Scanner;
+public class testdata3 {
+  public static void main(String[] args)
+    {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Input a number: ");
+        double input = in.nextDouble();
+
+        System.out.println("Input value: " + input);
+        if (input > 0)
+        {
+            if (input < 1)
+            {
+                System.out.println("Positive small number");
+            }
+            else if (input > 1000000)
+            {
+                System.out.println("Positive large number");
+            }
+            else
+            {
+                System.out.println("Positive number");
+            }
+        }
+        else if (input < 0)
+        {
+            if (Math.abs(input) < 1)
+            {
+                System.out.println("Negative small number");
+            }
+            else if (Math.abs(input) > 1000000)
+            {
+                System.out.println("Negative large number");
+            }
+            else
+            {
+                System.out.println("Negative number");
+            }
+        }
+        else
+        {
+            System.out.println("Zero");
+        }
+    }
+}
