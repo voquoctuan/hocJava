35bd92db2b9018647411080bad5c510a45df3129
voquoctuan <voquoctuanvfedu@gmail.com>
2022-03-04 07:30:27 +0700

    display the name of the weekday

---
+++ testdata4.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+public class testdata4 {
+
+
+  public static void main(String[] args)
+    {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Input number: ");
+        int day = input.nextInt();
+
+        System.out.println(getDayName(day));
+    }
+
+    public static String getDayName(int day) {
+        String dayName = "";
+        switch (day) {
+            case 1: dayName = "Monday"; break;
+            case 2: dayName = "Tuesday"; break;
+            case 3: dayName = "Wednesday"; break;
+            case 4: dayName = "Thursday"; break;
+            case 5: dayName = "Friday"; break;
+            case 6: dayName = "Saturday"; break;
+            case 7: dayName = "Sunday"; break;
+            default:dayName = "Invalid day range";
+        }
+
+        return dayName;
+    }
+}
