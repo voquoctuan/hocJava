496711348ac319d0d4c110995c1fae6ced41875c
voquoctuan <voquoctuanvfedu@gmail.com>
2022-03-04 10:10:20 +0700

    loop excercise 5

---
+++ loopEx5.java
@@ -0,0 +1,8 @@
+public class loopEx5 {
+    public static void main(String[] string) {
+        System.out.println("Display numbers from -10 to -1 using for loop");
+        for (int i = -10; i <= -1; i++) {
+            System.out.print(i + " ");
+        }
+    }
+}
