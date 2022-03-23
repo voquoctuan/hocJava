9d3d286db69d78ac38cc1e45bd7ee184219555ec
voquoctuan <voquoctuanvfedu@gmail.com>
2022-03-23 11:02:52 +0700

    Bai tap chuoi bai 3

---
+++ chuoi3.java
@@ -0,0 +1,8 @@
+public class chuoi3 {
+    public static void main(String[] args) {
+        char[] ch = {'H', 'e', 'l', 'l', 'o', 'c', 'a', 'c', 'b', 'a', 'n'};
+        String s = new String(ch);
+
+        System.out.println(s);
+    }
+}
